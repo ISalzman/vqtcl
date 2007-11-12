e65e7d4b5e06d1b602e83f83fc5e0ea76dd9fe85
Jean-Claude Wippler <jc@wippler.nl>
2007-11-12 22:31:37 +0000

    cleanup

--- v5/vq/tests/all.tcl
+++ v5/vq/tests/all.tcl
@@ -5,3 +5,6 @@
 #puts [info patchlevel]
 source [file join [file dir [info script]] initests.tcl]
 runAllTests
+
+# get rid of all single-character variables to release any associated memory
+eval unset [info vars ?]
