2e4c5e38e4b0b5788a5d2b9f9bb899ad81b50489
Jean-Claude Wippler <jc@wippler.nl>
2007-12-01 02:59:08 +0000

    tweak

--- v6/lvq/src/lvq.lua
+++ v6/lvq/src/lvq.lua
@@ -1,9 +1,9 @@
---[[  $Id: $
+--[[  $Id$
       LuaVlerq wrapper.  
 ]]--  This file is part of LuaVlerq.  See lvq.h for copyright notice.
 
 local core = require "lvq.core"
 
 module ("lvq", package.seeall)
- 
+
 assert(_VERSION == "LuaVlerq 1.6.0")
