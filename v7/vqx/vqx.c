66931f1ee54a7573329e29b630f483bfb0cb30cf
Jean-Claude Wippler <jc@wippler.nl>
2008-01-13 21:26:36 +0000

    add vqx

---
+++ v7/vqx/vqx.c
@@ -0,0 +1,18 @@
+#include "lvq.c"
+
+int main (int argc, char **argv) {
+    lua_State *L = lua_open();
+    luaL_openlibs(L);
+
+    lua_pushcfunction(L, luaopen_lvq_core);
+    lua_pushstring(L, "lvq.core");
+    lua_call(L, 1, 0);
+    
+    if (argc > 1 && luaL_dofile(L, argv[1]) != 0) {
+        fprintf(stderr,"%s\n", lua_tostring(L, -1));
+        return 1;
+    }
+    
+    lua_close(L);
+    return 0;
+}
